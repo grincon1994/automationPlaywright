0eb9ea334b0854c94eed87bc0726aaeea1656508
Gilberto Rincon <rincog3@medtronic.com>
2025-12-05 13:47:07 -0500

    adding items to cart logic

---
+++ src/pages/searchItemsAndAddToCart.ts
@@ -0,0 +1,17 @@
+import {Page, expect} from '@playwright/test';
+
+export class SearchItemsAndAddToCartPage{
+    readonly page; 
+
+    constructor(page:Page){
+        this.page = page;
+    }
+
+    async goto(){
+        await this.page.goto('https://automationexercise.com/');
+    }
+
+
+    async addItemsToCart(){
+
+    }
